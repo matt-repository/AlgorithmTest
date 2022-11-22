4cc3fb22c614cc98c5545f435b7d1f8338cc5562
fengq_Msi <1324396762@qq.com>
2022-11-22 16:00:31 +0800

    算法题 提交

---
+++ Sort/bubbleSort.go
@@ -0,0 +1,24 @@
+package Sort
+
+func BubbleSort1(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
+
+func BubbleSort2(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module AlgorithmTest
 
 go 1.17
+
+require github.com/Jeffail/tunny v0.1.4
